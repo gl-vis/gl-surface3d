9ef4cc4c0175c96d727b892257365d3eb2958558
Mikola Lysenko <mikolalysenko@gmail.com>
2014-08-05 17:35:16 -0500

    changed clipBounds to data coordinates

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -13,6 +13,6 @@ void main() {
   vec4 worldPosition = model * vec4(uv.zw, f, 1.0);
   gl_Position = projection * view * worldPosition;
   value = f;
-  worldCoordinate = worldPosition.xyz / worldPosition.w;
+  worldCoordinate = vec3(uv.zw, f);
   planeCoordinate = uv.xy;
 }
